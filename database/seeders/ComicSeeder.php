172cf1071ccebb8b6a587260871661677ea62980
Francesco Punta <puntafrancesco@gmail.com>
2023-04-16 10:02:45 +0200

    Seeder

--- database/seeders/ComicSeeder.php
+++ database/seeders/ComicSeeder.php
@@ -2,18 +2,26 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Comic;
 
 class ComicSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
-        //
+        $comics = require 'comics.php';
+
+        foreach ($comics as $comic) {
+            Comic::create([
+                'title' => $comic['title'],
+                'description' => $comic['description'],
+                'thumb' => $comic['thumb'],
+                'price' => $comic['price'],
+                'series' => $comic['series'],
+                'sale_date' => $comic['sale_date'],
+                'type' => $comic['type']
+            ]);
+        }
     }
 }
+
