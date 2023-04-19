f7ea32012d43b53b203d288de522df0f2b524bd7
Francesco Punta <puntafrancesco@gmail.com>
2023-04-19 15:58:06 +0200

    If error + style bootstrap form

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -2,37 +2,57 @@
 
 @section('content')
     <div class="container">
-        <h1>Create a New Comic</h1>
-        <form method="POST" action="{{ route('comics.store') }}">
+        <h1>Create New Comic</h1>
+
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
+        <form action="{{ route('comics.store') }}" method="POST">
             @csrf
+
             <div class="form-group">
                 <label for="title">Title</label>
-                <input type="text" class="form-control" id="title" name="title" required>
-                <div class="form-group">
-    <label for="description">Description</label>
-    <textarea class="form-control" id="description" name="description" rows="3" required></textarea>
-</div>
-<div class="form-group">
-    <label for="thumb">Thumbnail URL</label>
-    <input type="text" class="form-control" id="thumb" name="thumb" required>
-</div>
-<div class="form-group">
-    <label for="price">Price</label>
-    <input type="text" class="form-control" id="price" name="price" required>
-</div>
-<div class="form-group">
-    <label for="series">Series</label>
-    <input type="text" class="form-control" id="series" name="series" required>
-</div>
-<div class="form-group">
-    <label for="sale_date">Sale Date</label>
-    <input type="date" class="form-control" id="sale_date" name="sale_date" required>
-</div>
-<div class="form-group">
-    <label for="type">Type</label>
-    <input type="text" class="form-control" id="type" name="type" required>
-</div>
-<button type="submit" class="btn btn-primary">Submit</button>
-</form>
-</div>
+                <input type="text" class="form-control" id="title" name="title" value="{{ old('title') }}" required>
+            </div>
+
+            <div class="form-group">
+                <label for="description">Description</label>
+                <textarea class="form-control" id="description" name="description" rows="3" required>{{ old('description') }}</textarea>
+            </div>
+
+            <div class="form-group">
+                <label for="thumb">Thumbnail URL</label>
+                <input type="url" class="form-control" id="thumb" name="thumb" value="{{ old('thumb') }}" required>
+            </div>
+
+            <div class="form-group">
+                <label for="price">Price</label>
+                <input type="number" class="form-control" id="price" name="price" value="{{ old('price') }}" required>
+            </div>
+
+            <div class="form-group">
+                <label for="series">Series</label>
+                <input type="text" class="form-control" id="series" name="series" value="{{ old('series') }}" required>
+            </div>
+
+            <div class="form-group">
+                <label for="sale_date">Sale Date</label>
+                <input type="date" class="form-control" id="sale_date" name="sale_date" value="{{ old('sale_date') }}" required>
+            </div>
+
+            <div class="form-group">
+                <label for="type">Type</label>
+                <input type="text" class="form-control" id="type" name="type" value="{{ old('type') }}" required>
+            </div>
+
+            <button type="submit" class="btn btn-primary">Create Comic</button>
+        </form>
+    </div>
 @endsection
